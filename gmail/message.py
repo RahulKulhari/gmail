af9bc04f25a86f581f3d927bc3206f02e07244e0
Rahul Kulhari <rkulhari.live@gmail.com>
2014-11-14 14:21:41 +0530

    fix

--- gmail/message.py
+++ gmail/message.py
@@ -171,7 +171,7 @@ class Message():
             self.message_id = re.search(r'X-GM-MSGID (\d+)', raw_headers).groups(1)[0]
 
         print "subject is ",self.subject
-        Parse attachments into attachment objects array for this message
+        print "Parse attachments into attachment objects array for this message"
         self.attachments = [
             Attachment(attachment) for attachment in self.message._payload
                 if not isinstance(attachment, basestring) and attachment.get('Content-Disposition') is not None
